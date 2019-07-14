9cf23871354fe561c5bc4280a711d500665a55d4
Felipe Queiroz <spqfelipe@gmail.com>
2019-07-14 19:51:15 -0300

    grade pronta -- proximo share

--- gif_explorer/lib/Services/gif_service.dart
+++ gif_explorer/lib/Services/gif_service.dart
@@ -1,22 +1,35 @@
 import "dart:convert";
 import "package:http/http.dart" as http;
 
+// ############################
+// >> Gif Service 
+// Uses http module to connect to Giphy API
+
 class GifService{
 
   String _search;
-  String _offset;
+  int _offset = 0;
 
   // Constructor 
 
   // Gets and Sets
 
   String getSearch() => _search;
-  void setSearch(String value) { _search = value; }
-   
-  String getOffset() => _offset;
-  void setOffset(String value) { _offset = value; }
 
-  Future<Map> _getGifs() async {
+  void setSearch(String value) { _search = value; }
+
+  bool isSearch(){
+    if (_search == null) return false;
+    else return true;
+  }
+   
+  int getOffset() => _offset;
+
+  void setOffset(int value) { _offset = value; }
+
+  void incrementOffset(int value) {_offset += value;}
+
+  Future<Map> getGifs() async {
     http.Response response;
 
     if (_search == null)
@@ -24,8 +37,14 @@ class GifService{
           "https://api.giphy.com/v1/gifs/trending?api_key=CkYNcuKD69Vsr3rqJPILiWEsBe9ffoGP&limit=20&rating=G");
     else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=CkYNcuKD69Vsr3rqJPILiWEsBe9ffoGP&q=$_search&limit=25&offset=$_offset&rating=G&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=CkYNcuKD69Vsr3rqJPILiWEsBe9ffoGP&q=$_search&limit=19&offset=$_offset&rating=G&lang=en");
 
     return json.decode(response.body);
   }
+
+  int getCount(List data) {
+    if(_search == null) return data.length;
+    else return data.length + 1;
+  }
+
 }
--- gif_explorer/lib/UI/home_page.dart
+++ gif_explorer/lib/UI/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import "dart:convert";
-import "package:http/http.dart" as http;
+import 'package:gif_explorer/Services/gif_service.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,8 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String _search;
-  String _offset;
+  GifService gifService = GifService();
 
   // Widgets Functions
 
@@ -24,58 +22,90 @@ class _HomePageState extends State<HomePage> {
               border: OutlineInputBorder()),
           style: TextStyle(color: Colors.white, fontSize: 20.0),
           textAlign: TextAlign.center,
-          onSubmitted: (text){
+          onSubmitted: (text) {
             // Set state will force FutureBuilder to rebuild
             setState(() {
-              _search = text;
+              gifService.setSearch(text);
             });
           },
         ),
       );
 
-  Widget _buildGifTabel(BuildContext context, AsyncSnapshot snapshot) => GridView.builder(
-      padding: EdgeInsets.all(12.0),
-      // ! Grid Delegate is responsible to setup how the data will be displayed inside 
-      // ! the grid itself 
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 12.0,
-        mainAxisSpacing: 12.0,
-      ),
-      itemCount: 4,
-      itemBuilder: (context, index){
-        return GestureDetector(
-          child: Image.network(snapshot.data['data'][index]['images']['fixed_height']['url'],
-          height: 300.0,
-          fit: BoxFit.cover
+  Widget _buildLoadMoreCell() => Container(
+        child: GestureDetector(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 70.0,
+              ),
+              Text(
+                "More",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              )
+            ],
           ),
-        );
-      },
-  );
+          onTap: (){
+            setState(() {
+              gifService.incrementOffset(19);
+            });
+          },
+        ),
+      );
 
-  // ! API functions -- Remove later
+  Widget _buildGifTabel(BuildContext context, AsyncSnapshot snapshot) =>
+      GridView.builder(
+        padding: EdgeInsets.all(12.0),
+        // ! Grid Delegate is responsible to setup how the data will be displayed inside
+        // ! the grid itself
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+        ),
+        itemCount: gifService.getCount(snapshot.data['data']),
+        itemBuilder: (context, index) {
+          if (!gifService.isSearch() || index < snapshot.data['data'].length) {
+            return GestureDetector(
+              child: Image.network(
+                  snapshot.data['data'][index]['images']['fixed_height']['url'],
+                  height: 300.0,
+                  fit: BoxFit.cover),
+            );
+          } else {
+            return _buildLoadMoreCell();
+          }
+        },
+      );
 
-  Future<Map> _getGifs() async {
-    http.Response response;
-
-    if (_search == null)
-      response = await http.get(
-          "https://api.giphy.com/v1/gifs/trending?api_key=CkYNcuKD69Vsr3rqJPILiWEsBe9ffoGP&limit=20&rating=G");
-    else
-      response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=CkYNcuKD69Vsr3rqJPILiWEsBe9ffoGP&q=$_search&limit=25&offset=$_offset&rating=G&lang=en");
-
-    return json.decode(response.body);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    // _getGifs().then((map) {
-    //   print(map);
-    // });
-  }
+  Widget _buildGifBody() => Expanded(
+        child: FutureBuilder(
+          future: gifService.getGifs(),
+          builder: (context, snapshot) {
+            // Verify the connection state to display propper data
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return Container(
+                  height: 300.0,
+                  width: 300.0,
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    strokeWidth: 4.0,
+                  ),
+                );
+              default:
+                if (snapshot.hasError)
+                  return Container();
+                else
+                  return _buildGifTabel(context, snapshot);
+            }
+          },
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -89,30 +119,7 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           children: <Widget>[
             _buildSearchField(),
-            Expanded(
-              child: FutureBuilder(
-                future: _getGifs(),
-                builder: (context, snapshot){
-                  // Verify the connection state to display propper data
-                  switch(snapshot.connectionState){
-                    case ConnectionState.waiting:
-                    case ConnectionState.none:
-                      return Container(
-                        height: 300.0,
-                        width: 300.0,
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                          strokeWidth: 4.0,
-                        ),
-                      );
-                    default:
-                      if(snapshot.hasError) return Container();
-                      else return _buildGifTabel(context, snapshot);
-                  }
-                },
-              ),
-            ),
+            _buildGifBody(),
           ],
         ));
   }
